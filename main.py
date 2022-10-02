cb826273e4653aec0ba5cf0996ce7fccae644c8d
cdalphapt <106878063+cdalphapt@users.noreply.github.com>
2022-10-02 12:19:57 +0800

    Add files via upload
    v0.2 
    -Successfully segment the reference part of the paper.
    (Notice that the program just find the last "reference" word in the paper, so there's a chance that the segmentation fail)

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 
 if __name__ == '__main__':
     originPaper = utils.pdfPaper('./2207.06103v2.pdf')
+    originPaper.FindRefs()
 
 
 
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
 import PyPDF2
+import string
+import re
 
 
 class pdfPaper:
@@ -8,5 +10,19 @@ class pdfPaper:
         self.dir = dir
         pdfReader = PyPDF2.PdfFileReader(open(dir, 'rb'))
         self.title = pdfReader.getDocumentInfo().title
+        self.contentTxt = ""
+        self.refs = []
+        for page in pdfReader.pages:
+            self.contentTxt += page.extract_text()
+        print(self.contentTxt)
+
+    def FindRefs(self):
+        tempLowercTxt = self.contentTxt.lower()
+        tempRefStartContainer = [_.start() for _ in re.finditer('references', tempLowercTxt)]
+        print()
+        print(self.contentTxt[tempRefStartContainer[-1]::1])
+        #for page in pdfReader.getFields()
+        #self.refs = []
+
         #print(self.name)
     pass
