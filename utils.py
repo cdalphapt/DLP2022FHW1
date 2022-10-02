dc95f06a7afba90e7119a805e12886f3cdd74e37
cdalphapt <106878063+cdalphapt@users.noreply.github.com>
2022-10-02 14:53:09 +0800

    Add files via upload
    v0.3
    -Successfully output all references (though extremely limited)

--- utils.py
+++ utils.py
@@ -18,11 +18,41 @@ class pdfPaper:
 
     def FindRefs(self):
         tempLowercTxt = self.contentTxt.lower()
-        tempRefStartContainer = [_.start() for _ in re.finditer('references', tempLowercTxt)]
-        print()
-        print(self.contentTxt[tempRefStartContainer[-1]::1])
-        #for page in pdfReader.getFields()
-        #self.refs = []
+        tempRefContainer = [subStr.start() for subStr in re.finditer('references', tempLowercTxt)]
+        tempAllRefs = self.contentTxt[tempRefContainer[-1]::1]#Find last "references"
+        tempAllRefs = FormatCorrection(tempAllRefs)
+        tempRefContainer = [subStr.start() for subStr in re.finditer('\[[0-9]+\]', tempAllRefs)]
+        for i in range(len(tempRefContainer)):
+            if i == len(tempRefContainer) - 1:
+                tempRef = tempAllRefs[tempRefContainer[i]+2+len(str(i))::1]
+                tempRef = tempRef.strip()
+                self.refs.append(tempRef)
+            else:
+                tempRef = tempAllRefs[tempRefContainer[i]+2+len(str(i)):tempRefContainer[i+1]:1]
+                tempRef = tempRef.strip()
+                self.refs.append(tempRef)
+        for ref in self.refs:
+            print('--------------------------------------------------------------------------')
+            print(ref)
 
-        #print(self.name)
-    pass
+            
+
+
+
+        #req = urllib.request.Request('https://dblp.uni-trier.de/search//publ/inc?q=Offline%20Reinforcement%20Learning&s=ydvspc&h=30&b='+str(j))
+        #response = urllib.request.urlopen(req)
+        #the_page = response.read().decode('utf-8')
+
+def FormatCorrection(oriStr):
+    #correct the format for later process
+    tempPosList = [subStr.start() for subStr in re.finditer(r'-\n[a-z]', oriStr)]
+    #Clear hyphen, but keep en-dash unchanged (hpoefully)
+    #print(connectPosList)
+    tempPosList = tempPosList[-1::-1]
+    for subStrIndex in tempPosList:#del should start from tail
+        oriStr = oriStr[0:subStrIndex:1] + oriStr[subStrIndex+2::1]
+    #print(oriStr)
+    oriStr = oriStr.replace('\n', '')
+    #print(oriStr)
+    return oriStr
+
