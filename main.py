93ef3c0352d43fd0b8095c3ee49f34ccecc0d717
cdalphapt <106878063+cdalphapt@users.noreply.github.com>
2022-10-02 19:11:37 +0800

    v0.5
    -Core function finished. Now the program can find paper's bib info and download. If there are no more than 5 matches (and of cource more than 0), the program then choose one of the possible match. 
    -Plus a debug function. Use "python main.py -d" to let the program output more information about title segmentation, etc.

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+from getopt import getopt
 import utils
 import urllib.request
 import re
 import os
+import sys
+
+debug = 0
+opts, args =  getopt(sys.argv[1:], 'd')
+for op, value in opts:
+    if op == "-d":
+        debug = 1;
 
 if __name__ == '__main__':
-    originPaper = utils.pdfPaper('./2207.06103v2.pdf')
+    originPaper = utils.pdfPaper('./1904.05939.pdf')
     originPaper.FindRefs()
     originPaper.ShowRefs()
-    utils.SearchForPaper(originPaper.refs[0])
+    originPaper.DownloadAllPapers()
 
 
 
--- utils.py
+++ utils.py
@@ -1,10 +1,16 @@
+from base64 import encode
 import os
+from urllib import request
 import PyPDF2
 import string
 import re
 import urllib3
 import requests
 from bs4 import BeautifulSoup
+import time
+from main import debug      
+
+foundStr = ['found one match','found 2 matches', 'found 3 matches', 'found 4 matches', 'found 5 matches']
 
 
 class pdfPaper:
@@ -13,11 +19,13 @@ class pdfPaper:
         self.dir = dir
         pdfReader = PyPDF2.PdfFileReader(open(dir, 'rb'))
         self.title = pdfReader.getDocumentInfo().title
+        print(self.title)
         self.contentTxt = ""
         self.refs = []
         for page in pdfReader.pages:
             self.contentTxt += page.extract_text()
-        print(self.contentTxt)
+        if debug == 1:
+            print(self.contentTxt)
 
     def FindRefs(self):
         tempLowercTxt = self.contentTxt.lower()
@@ -34,6 +42,10 @@ class pdfPaper:
                 tempRef = tempAllRefs[tempRefContainer[i]+2+len(str(i+1)):tempRefContainer[i+1]:1]
                 tempRef = tempRef.strip()
                 self.refs.append(tempRef)
+
+    def DownloadAllPapers(self):
+        for ref in self.refs:
+            SearchForPaper(ref)
 
     def ShowRefs(self):
         for ref in self.refs:
@@ -66,9 +78,12 @@ def FormatCorrection(oriStr):
 
 def SearchForPaper(paperName):
     #1:Segment name by dot
+    print('--------------------------------------------------------------------------')
+    print("Downloading:\n" + paperName)
     tempNamePool = []
     tempPosList = [subStr.start() for subStr in re.finditer(r'\. ', paperName)]
-    print(tempPosList)
+    if debug == 1:
+        print(tempPosList)
     for i in range(len(tempPosList)):
         if i == len(tempPosList) - 1:
             tempStr = paperName[tempPosList[i]+1::1]
@@ -79,7 +94,9 @@ def SearchForPaper(paperName):
             tempStr = tempStr.strip()
             tempNamePool.append(tempStr)
     tempNamePool = tempNamePool[-1::-1]
-    print(tempNamePool)
+    if debug == 1:
+        print(tempNamePool)
+    IsFindPaper = 0
     for segName in tempNamePool:
         #http = urllib3.PoolManager()
         #response = http.request('GET', 'https://dblp.org/search?q=' + segName)
@@ -87,8 +104,80 @@ def SearchForPaper(paperName):
         #data = response.data.decode('utf-8')
         url = 'https://dblp.org/search?q=' + segName
         bs = BeautifulSoup(requests.get(url).content, "lxml")
-        result = bs.find('p', id = "completesearch-info-matches").get_text()
-        print(result)
+        result = bs.find('p', id = 'completesearch-info-matches').get_text()
+        if debug == 1:
+            print(result)
+        if not foundStr.__contains__(result):
+            continue
+        else:
+            ul_1 = bs.find('ul', class_ = 'publ-list')
+            nav = ul_1.find('nav', class_ = 'publ')
+            navTxt = str(nav)
+            #find BibTex
+            texPos = navTxt.find("BibTeX")
+            closeAStart,closeAEnd = len(navTxt),len(navTxt)
+            #Find <a></a> bibtex belong
+            for subStr in re.finditer('<a', navTxt[0:texPos:1]):
+                if abs(subStr.start() - texPos) <= abs(closeAStart - texPos):
+                    closeAStart = subStr.start()
+            for subStr in re.finditer('</a>', navTxt[texPos::1]):
+                if abs(subStr.start()) <= abs(closeAEnd):
+                    closeAEnd = subStr.start()
+            subBibTeXStr = navTxt[closeAStart:closeAEnd+texPos:1]
+            herfPos = subBibTeXStr.find("href=")
+            linkS,linkE = -1,-1
+            i = herfPos
+            for char in subBibTeXStr[herfPos::1]:
+                i += 1
+                if char == '\"':
+                    if linkS == -1:
+                        linkS = i
+                    elif linkE == -1:
+                        linkE = i
+                        break
+            herfLink = subBibTeXStr[linkS:linkE-1:1]
+            #find bib download link
+            bibWeb = str(BeautifulSoup(requests.get(herfLink).content, "lxml"))
+            texPos = str(bibWeb).find("download as .bib file")
+            closeAStart,closeAEnd = len(bibWeb),len(bibWeb)
+            #Find <a></a> bib belong
+            for subStr in re.finditer('<a', bibWeb[0:texPos:1]):
+                if abs(subStr.start() - texPos) <= abs(closeAStart - texPos):
+                    closeAStart = subStr.start()
+            for subStr in re.finditer('</a>', bibWeb[texPos::1]):
+                if abs(subStr.start()) <= abs(closeAEnd):
+                    closeAEnd = subStr.start()
+            subBibTeXStr = bibWeb[closeAStart:closeAEnd+texPos:1]
+            herfPos = subBibTeXStr.find("href=")
+            linkS,linkE = -1,-1
+            i = herfPos
+            for char in subBibTeXStr[herfPos::1]:
+                i += 1
+                if char == '\"':
+                    if linkS == -1:
+                        linkS = i
+                    elif linkE == -1:
+                        linkE = i
+                        break
+            bibDownloadLink = subBibTeXStr[linkS:linkE-1:1]
+            bib = requests.get(bibDownloadLink)
+            f = open('./download/' + str(time.time()) + '.bib', 'wb')
+            f.write(bib.content)
+            f.close()
+            print("Download Success")
+            IsFindPaper = 1
+            return 1;
+    if IsFindPaper == 0:
+        print("Paper not Found")
+        return 0;
+
+
+            #bibPos = nav.
+
+            #print(bibPos)
+        #Find the only paper suitable
+        
+
         #searched = bs.find(class_ = "publ-list")
         #ul = searched.find('ul')
         #print(ul)
